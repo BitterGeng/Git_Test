508361701a9b4eef7a41c60cd0109e9e11a6e286
BitterGeng <839407658@qq.com>
2018-08-24 17:46:25 +0800

    新建一个仓库

---
+++ src/main/java/com/ttp/Person.java
@@ -0,0 +1,47 @@
+package com.ttp;
+
+/**
+ * Created by songbogeng on 2018/8/24.
+ */
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    private String sex;
+
+    private String weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+}
