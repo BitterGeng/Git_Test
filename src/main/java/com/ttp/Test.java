7219435d0e9157d1cf9ed80c7fcf4b1380a9fc2d
BitterGeng <839407658@qq.com>
2018-08-24 17:48:45 +0800

    add test.class

---
+++ src/main/java/com/ttp/Test.java
@@ -0,0 +1,12 @@
+package com.ttp;
+
+/**
+ * Created by Administrator on 2018/8/24.
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
