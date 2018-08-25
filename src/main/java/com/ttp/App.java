9c62e8528e3b39faf42b0038c4bb8790fca78433
BitterGeng <839407658@qq.com>
2018-08-25 11:00:47 +0800

    bran1修改test类

--- src/main/java/com/ttp/App.java
+++ src/main/java/com/ttp/App.java
@@ -2,12 +2,17 @@ package com.ttp;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("Hello World!");
+
+        String str = null;
+        str = String.format("Hi,%s", "飞龙");          // 格式化字符串
+
+        double n = 3.1415926;
+        String s = String.format("%f %n");
+        System.out.println(s);
     }
 }
